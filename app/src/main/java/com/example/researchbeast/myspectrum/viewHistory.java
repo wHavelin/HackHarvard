ed32fa009fff555422806384e982b7c4e04e4996
Daan Leiva <daan.leiva@gmail.com>
2015-11-14 10:00:39 -0800

    Added sharedPreferences

--- app/src/main/java/com/example/researchbeast/myspectrum/viewHistory.java
+++ app/src/main/java/com/example/researchbeast/myspectrum/viewHistory.java
@@ -1,6 +1,8 @@
 package com.example.researchbeast.myspectrum;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,15 +10,28 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.researchbeast.myspectrum.models.NewEventModel;
+import com.google.gson.Gson;
+
+import org.w3c.dom.Text;
+
+import java.util.Map;
+import java.util.Set;
 
 import static com.example.researchbeast.myspectrum.R.id.eventListView;
 
 public class viewHistory extends AppCompatActivity {
 
+    SharedPreferences mprefs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        String[] myStringArray = {"something", "somethingElse"};
+        mprefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String[] myStringArray = {"something", "somethingElse", "third", "fgd"};
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, myStringArray);
@@ -39,6 +54,16 @@ public class viewHistory extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
 
+        // get shared settings
+        Map<String, ?> entries = mprefs.getAll();
+        Set<String> keys = entries.keySet();
+        Gson gson = new Gson();
+
+        for(String key : keys) {
+            NewEventModel model =  gson.fromJson((String) entries.get(key), NewEventModel.class);
+
+            //
+        }
+    }
 }

ca4554cfd0d090f4935641dbcbc0dffc490a5d85
zujko <zujko@csh.rit.edu>
2015-11-14 12:09:18 -0500

    Open NewEventActivity on button click

--- app/src/main/java/com/example/researchbeast/myspectrum/MainActivity.java
+++ app/src/main/java/com/example/researchbeast/myspectrum/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.researchbeast.myspectrum;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.facebook.shimmer.ShimmerFrameLayout;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.newEventBtn) Button mNewEventButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         // run shimmer animation
         ShimmerFrameLayout container = (ShimmerFrameLayout) findViewById(R.id.shimmer_view_container);
@@ -39,5 +47,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onNewEventClick(View view) {
+        startActivity(new Intent(this, NewEventActivity.class));
     }
 }

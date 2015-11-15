7399dd9a2a495ef44f97f3a113ae002802c4fc92
zujko <zujko@csh.rit.edu>
2015-11-15 01:20:28 -0500

    Added empty activity

---
+++ app/src/main/java/com/example/researchbeast/myspectrum/activities/CardViewActivity.java
@@ -0,0 +1,25 @@
+package com.example.researchbeast.myspectrum.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.example.researchbeast.myspectrum.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class CardViewActivity extends AppCompatActivity {
+    @Bind(R.id.toolbar) Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_view);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+
+    }
+
+}
